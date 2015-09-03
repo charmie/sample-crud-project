0ecccd1b4782f096a7fa7db0c50d3a7b062d32b0
csorila17@gmail.com <csorila17@gmail.com>
2015-09-03 11:04:56 +0800

    Modified login page to include register button

--- resources/views/accounts/login.blade.php
+++ resources/views/accounts/login.blade.php
@@ -3,20 +3,22 @@
 @section('content')
 	<h1>User Login</h1>
 	<hr />
-
-	
 	{!! Form::open(array('url' => 'accounts/userLogin')) !!}
-    
-
 	    {!! Form::text('username') !!}
 	    {!! Form::password('password') !!}
 	    {!! Form::submit('Login') !!}
 	{!! Form::close() !!}
-	
-	
+
+	<br /><br />
+	<button id="btn-register">Register</button>
 @stop
 
 @section('javascript')
-    
-
+<script>
+	jQuery(document).ready(function(){
+		jQuery('#btn-register').click(function(){
+			window.location = 'accounts/registration';
+		});	
+	})
+</script>
 @stop
